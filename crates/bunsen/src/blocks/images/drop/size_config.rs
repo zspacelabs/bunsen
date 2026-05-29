24341b4ca1530fbe0b0b8b5859814471eb08d7b7
zyc <109252977+YichiZhang0613@users.noreply.github.com>
2026-05-29 22:19:41 +0800

    Fix Inconsistency

--- crates/bunsen/src/blocks/images/drop/size_config.rs
+++ crates/bunsen/src/blocks/images/drop/size_config.rs
@@ -46,7 +46,7 @@ impl SizeConfig {
         match self {
             SizeConfig::Default => source_size,
             SizeConfig::Ratio(ratio) => {
-                assert!(ratio >= 0.0, "Ratio must be positive: {ratio}");
+                assert!(ratio >= 0.0, "Ratio must be non-negative: {ratio}");
                 ((source_size as f64) * ratio) as usize
             }
             SizeConfig::Fixed(size) => size,
