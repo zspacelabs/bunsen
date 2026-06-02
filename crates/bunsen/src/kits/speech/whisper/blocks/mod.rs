81a914b805fa21bcebe837b631e3af784de100f4
Crutcher Dunnavant <crutcher@gmail.com>
2026-06-02 14:46:51 -0700

    Lift whisper model blocks into whisper::blocks.

---
+++ crates/bunsen/src/kits/speech/whisper/blocks/mod.rs
@@ -0,0 +1,18 @@
+//! Whisper Component Blocks
+
+mod audio_encoder;
+mod decoder_block;
+mod encoder_block;
+mod text_decoder;
+mod whisper_model;
+
+#[doc(inline)]
+pub use audio_encoder::*;
+#[doc(inline)]
+pub use decoder_block::*;
+#[doc(inline)]
+pub use encoder_block::*;
+#[doc(inline)]
+pub use text_decoder::*;
+#[doc(inline)]
+pub use whisper_model::*;
