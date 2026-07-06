24e3bceeb7e98b8e842cefb8b96407501880df0e
Crutcher Dunnavant <crutcher@gmail.com>
2026-07-06 14:24:57 -0700

    refactor[silero_vad]: rename `container` module to `collection`, add `forward_sequence` method, and improve method documentation

--- crates/bunsen/src/kits/speech/silero_vad/blocks/collection.rs
+++ crates/bunsen/src/kits/speech/silero_vad/blocks/collection.rs
@@ -17,7 +17,10 @@ pub struct SileroVadCollection<B: Backend> {
 }
 
 impl<B: Backend> SileroVadCollection<B> {
-    fn branch(
+    /// Select the branch for the given signal rate.
+    ///
+    /// Panics if the sample rate is not supported.
+    pub fn expect_branch(
         &self,
         sr: usize,
     ) -> &SileroVad<B> {
@@ -29,30 +32,48 @@ impl<B: Backend> SileroVadCollection<B> {
     }
 
     /// Forward.
+    ///
+    /// See: [`SileroVad::forward`].
     pub fn forward(
         &self,
         sr: usize,
         input: Tensor<B, 2>,
         state: Tensor<B, 3>,
     ) -> (Tensor<B, 1>, Tensor<B, 3>) {
-        self.branch(sr).forward(input, state)
+        self.expect_branch(sr).forward(input, state)
+    }
+
+    /// Forward Sequence.
+    ///
+    /// See: [`SileroVad::forward_sequence`].
+    pub fn forward_sequence(
+        &self,
+        sr: usize,
+        input: Tensor<B, 3>,
+        state: Tensor<B, 3>,
+    ) -> (Tensor<B, 2>, Tensor<B, 3>) {
+        self.expect_branch(sr).forward_sequence(input, state)
     }
 
     /// Initialize the state.
+    ///
+    /// See: [`SileroVad::init_state`].
     pub fn init_state(
         &self,
         sr: usize,
         batch_size: usize,
         device: &B::Device,
     ) -> Tensor<B, 3> {
-        self.branch(sr).init_state(batch_size, device)
+        self.expect_branch(sr).init_state(batch_size, device)
     }
 
     /// Chunk size for the given sample rate.
+    ///
+    /// See: [`SileroVad::chunk_size`].
     pub fn chunk_size(
         &self,
         sr: usize,
     ) -> usize {
-        self.branch(sr).chunk_size()
+        self.expect_branch(sr).chunk_size()
     }
 }
