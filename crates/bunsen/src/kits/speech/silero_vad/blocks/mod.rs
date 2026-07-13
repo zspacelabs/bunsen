5e89f0c4537268848285fef8ead1e257e37c9d62
Crutcher Dunnavant <crutcher@gmail.com>
2026-07-13 12:46:12 -0700

    refactor(silero): move SileroVadContext to a dif file

--- crates/bunsen/src/kits/speech/silero_vad/blocks/mod.rs
+++ crates/bunsen/src/kits/speech/silero_vad/blocks/mod.rs
@@ -1,11 +1,14 @@
 //! Silero VAD blocks
 
 mod collection;
+mod context;
 mod fused_lstm;
 mod module;
 
 #[doc(inline)]
 pub use collection::*;
+#[doc(inline)]
+pub use context::*;
 #[doc(inline)]
 pub use fused_lstm::*;
 #[doc(inline)]
