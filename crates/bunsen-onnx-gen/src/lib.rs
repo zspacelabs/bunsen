98e75cfbf1031d47ef90f4ac17b63aa23ae2397e
Crutcher Dunnavant <crutcher@gmail.com>
2026-07-13 23:26:02 -0700

    add ten-vad to onnx gen (#124)
    * add ten-vad to onnx gen
    
    * import
    
    * fix doc test

--- crates/bunsen-onnx-gen/src/lib.rs
+++ crates/bunsen-onnx-gen/src/lib.rs
@@ -3,6 +3,8 @@
 //! ## Crate Features
 #![doc = document_features::document_features!()]
 
+extern crate alloc;
+
 #[cfg(feature = "silero")]
 pub mod silero {
     include!(concat!(env!("OUT_DIR"), "/silero_vad_op18_ifless.rs"));
@@ -10,6 +12,25 @@ pub mod silero {
     /// Pretrained dual branch 16khz/8khs model.
     pub const BURNPACK_WEIGHTS: &[u8] =
         include_bytes!(concat!(env!("OUT_DIR"), "/silero_vad_op18_ifless.bpk"));
+
+    /// Convert the burnpack weights to a burn bytes.
+    pub fn burnpack_as_burn_bytes() -> burn::tensor::Bytes {
+        burn::tensor::Bytes::from_bytes_vec(BURNPACK_WEIGHTS.to_vec())
+    }
+
+    impl<B: Backend> Model<B> {
+        /// Load the pretrained model.
+        pub fn load_pretrained(device: &B::Device) -> Model<B> {
+            Model::from_bytes(burnpack_as_burn_bytes(), device)
+        }
+    }
+}
+
+#[cfg(feature = "ten")]
+pub mod ten {
+    include!(concat!(env!("OUT_DIR"), "/ten-vad.rs"));
+
+    pub const BURNPACK_WEIGHTS: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/ten-vad.bpk"));
 
     /// Convert the burnpack weights to a burn bytes.
     pub fn burnpack_as_burn_bytes() -> burn::tensor::Bytes {
